5a2b963adaae759f2b98b2ad55f8162fee0d6119
ojacome01 <ojacome@libelulasoft.com>
2021-06-19 18:10:41 -0500

    modelado de tablas

---
+++ app/Http/Controllers/EnterprisesController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\enterprises;
+use Illuminate\Http\Request;
+
+class EnterprisesController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\enterprises  $enterprises
+     * @return \Illuminate\Http\Response
+     */
+    public function show(enterprises $enterprises)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\enterprises  $enterprises
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(enterprises $enterprises)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\enterprises  $enterprises
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, enterprises $enterprises)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\enterprises  $enterprises
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(enterprises $enterprises)
+    {
+        //
+    }
+}
---
+++ database/factories/EnterprisesFactory.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\enterprises;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class EnterprisesFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = enterprises::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            //
+        ];
+    }
+}
